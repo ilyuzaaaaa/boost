31cabe8f3bb4c9ab32dc98b7be2b352e375af837
ilyuzaaaaa <ilyuza.yangirova@gmail.com>
2020-12-06 07:11:57 +0500

    new

---
+++ include/FileSort.hpp
@@ -0,0 +1,36 @@
+// Copyright 2020 Ilyuza Yangirova ilyuza.yangirova@gmail.com
+
+
+#ifndef INCLUDE_FILESORT_HPP_
+#define INCLUDE_FILESORT_HPP_
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <exception>
+#include <boost/filesystem.hpp>
+#include <utility>
+
+#define bf boost::filesystem
+
+class FileSort
+{
+    private:
+    bf::path ftp_path;
+    // Имя Брокера, Номер Счёта, Дата, Кол-во
+    std::map<std::string, std::map<int, std::pair<int, int>>> Brokers;
+    size_t NumBrokers;
+    public:
+    explicit FileSort(const bf::path& ftp_path);
+    std::map<std::string, std::map<int, std::pair<int, int>>> GetBrokers();
+
+    size_t GetNumBrokers();
+    void filer(const bf::path &path);
+    ~FileSort();
+};
+
+
+
+
+#endif // INCLUDE_FILESORT_HPP_
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,15 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Ilyuza Yangirova ilyuza.yangirova@gmail.com
 
-#include <header.hpp>
+#include <FileSort.hpp>
+
+int main(int argc, char *argv[]) {
+    bf::path ftp_path;
+    if (argc < 2)
+        ftp_path = "../";
+    else
+        ftp_path = std::string(argv[1]);
+
+ FileSort A(ftp_path);
+ std::cout<< A.GetBrokers().size();
+    return 0;
+}
